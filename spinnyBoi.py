5624d584943a231947a304c6a8c39876639f0797
tyleragostino <tyleragostino@gmail.com>
2023-06-12 15:18:44 -0400

    major refactoring

--- spinnyBoi.py
+++ spinnyBoi.py
@@ -1,14 +1,16 @@
+from math import isnan
 import os
 import logging
 import sys
 import discord
+import pandas
 from selenium import webdriver
 from PIL import Image
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 import time
 import datetime
-import typing # For typehinting
+import typing  # For type hinting
 import functools
 import asyncio
 import shutil
@@ -21,7 +23,7 @@ import io
 import requests
 import pandas as pd
 
-ghmsg = "Round 7"
+status_message = "Round 7"
 
 
 logger = logging.getLogger()
@@ -31,7 +33,8 @@ handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-greylist = [515926385731305502, 335864725600600064]
+grey_list = [515926385731305502, 335864725600600064]
+
 
 def message_handler(file='messages.txt'):
     roll = random.random()
@@ -40,17 +43,21 @@ def message_handler(file='messages.txt'):
     lines = len(messages)
     return messages[int(int(roll*100) % lines)]
 
+
 def get_message():
     return message_handler(file='messages.txt')
 
+
 def get_greylist():
     return message_handler(file='greymessages.txt')
+
 
 def to_thread(func: typing.Callable) -> typing.Coroutine:
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
         return await asyncio.to_thread(func, *args, **kwargs)
     return wrapper
+
 
 def get_info():
     with open("profiles.yaml") as profile_file:
@@ -69,7 +76,7 @@ def get_info():
 class MyClient(discord.Client):
     async def on_ready(self):
         logger.info('Logged on as ' + str(self.user))
-        activity = discord.Activity(name=ghmsg, type=discord.ActivityType.playing)
+        activity = discord.Activity(name=status_message, type=discord.ActivityType.playing)
         await self.change_presence(status=discord.Status.online, activity=activity)
 
     async def on_message(self, message):
@@ -77,11 +84,12 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        # greylist
-        if str(message.content).lower().startswith('/spin') and message.author.id in greylist and random.randrange(1, 100) <= 50:
+        # grey list
+        if str(message.content).lower().startswith('/spin') and message.author.id in grey_list and random.randrange(1, 100) <= 50:
             if random.randrange(1,100) <= 10:
                 original = await message.channel.send("Got it, one sec...")
-                response = requests.get('https://media4.giphy.com/media/LrmU6jXIjwziE/giphy.gif?cid=ecf05e47t84wtpzmxqziq5zugsn7ms53jg279h4lp6y9u1w1&ep=v1_gifs_related&rid=giphy.gif&ct=g')
+                url = 'https://media4.giphy.com/media/LrmU6jXIjwziE/giphy.gif?cid=ecf05e47t84wtpzmxqziq5zugsn7ms53jg279h4lp6y9u1w1&ep=v1_gifs_related&rid=giphy.gif&ct=g'
+                response = requests.get(url)
                 file = io.BytesIO(response.content)
                 file.name = 'upload.gif'
                 # time.sleep(1)
@@ -96,39 +104,6 @@ class MyClient(discord.Client):
         if str(message.content).lower() == '/spinfo':
             response_body = get_info()
             response = await message.channel.send(response_body)
-
-        if str(message.content).lower().startswith('/spin spreadsheet'):
-            full_filter_string = str(message.content.lower()).removeprefix('/spin spreadsheet').strip(' ')
-            tab = full_filter_string.split(' ')[0]
-            filter_string = ' '.join(full_filter_string.split(' ')[1:])
-            url = generate_spreadsheet_url(tab, filter_string)
-            original = await message.channel.send("Got it, one sec...")
-            if url is not None and not url.__contains__('?choices=&weights'):
-                file = await spin_dat_wheel(url)
-                if file is None:
-                    await original.edit(content='Something went wrong.')
-                else:
-                    fp = discord.File(file)
-                    await original.edit(content=message.author.mention + " " + get_message(), attachments=[fp])
-            else:
-                await original.edit(content='Something went wrong.')
-            return
-
-        if str(message.content).lower() == '/spin' or str(message.content).lower() == '/spin default':
-            url = generate_url('season4')
-            original = await message.channel.send("Got it, one sec...")
-            originaltwo = await message.channel.send("Please be patient, this will take a little longer than usual.")
-            file = await spin_dat_wheel(url)
-            if file is None:
-                await original.edit(content='Something went wrong.')
-            else:
-                # second follow-up wheel:
-                urltwo = generate_url('conditions')
-                filetwo = await spin_dat_wheel(urltwo)
-                fp = discord.File(file)
-                fptwo = discord.File(filetwo)
-                await original.edit(content=message.author.mention + " " + get_message(), attachments=[fp])
-                await originaltwo.edit(content=None, attachments=[fptwo])
 
         if str(message.content).lower().startswith('/spin custom '):
             try:
@@ -152,10 +127,25 @@ class MyClient(discord.Client):
             finally:
                 return
 
-        if str(message.content).lower().startswith('/spin '):
-            selected_profile = str(message.content)[5:].strip(' ')
-            url = generate_url(selected_profile)
+        if str(message.content).lower().startswith('/spin preset '):
             original = await message.channel.send("Got it, one sec...")
+            preset_name = str(message.content.lower()).removeprefix('/spin preset ').strip(' ')
+            urls = generate_preset_urls(preset_name)
+            files = []
+            for url in urls:
+                wheel = await spin_dat_wheel(url)
+                if wheel is not None:
+                    dicordfiles = discord.File(wheel)
+                    files.append(dicordfiles)
+            await original.edit(content=message.author.mention + " " + get_message(), attachments=files)
+            return
+
+        if str(message.content).lower().startswith('/spin '):
+            original = await message.channel.send("Got it, one sec...")
+            full_filter_string = str(message.content.lower()).removeprefix('/spin ').strip(' ')
+            tab = full_filter_string.split(' ')[0]
+            filter_string = ' '.join(full_filter_string.split(' ')[1:])
+            url = generate_spreadsheet_url(tab, filter_string)
             if url is not None and not url.__contains__('?choices=&weights'):
                 file = await spin_dat_wheel(url)
                 if file is None:
@@ -164,8 +154,29 @@ class MyClient(discord.Client):
                     fp = discord.File(file)
                     await original.edit(content=message.author.mention + " " + get_message(), attachments=[fp])
             else:
-                await message.channel.send('Sorry, I don\'t recognize that command.')
+                await original.edit(content='Something went wrong.')
+            return
 
+        if str(message.content).lower() == '/spin':
+            original = await message.channel.send("Got it, one sec...")
+            urls = generate_preset_urls('DEFAULT')
+            files = []
+            for url in urls:
+                wheel = await spin_dat_wheel(url)
+                if wheel is not None:
+                    dicordfiles = discord.File(wheel)
+                    files.append(dicordfiles)
+            await original.edit(content=message.author.mention + " " + get_message(), attachments=files)
+
+def generate_preset_urls(preset_name):
+    spreadsheet = os.getenv('GSHEET_ID')
+    url = f'https://docs.google.com/spreadsheets/d/{spreadsheet}/gviz/tq?tqx=out:csv&sheet=presets'
+    filters = pd.read_csv(url).query(f"Fullname.str.lower()=='{preset_name.lower()}'").to_dict('records')[0]
+    urls = []
+    for tabname in filters.keys():
+        if isinstance(filters[tabname], str) and tabname != 'Fullname':
+            urls.append(generate_spreadsheet_url(tabname, str(filters[tabname]).lower()))
+    return urls
 
 def get_value_from_nested_dict(d, s):
     keys = s.split('.')
@@ -175,7 +186,6 @@ def get_value_from_nested_dict(d, s):
         else:
             return None
     return {keys[-1]: d[keys[-1]]}
-
 
 
 def main():
@@ -199,54 +209,84 @@ def generate_url(profile):
     if selection is None:
         return None
 
-    option_sets = [y[selection[0]][option] for option in y[selection[0]] if option.upper() == selection[1].upper() or selection[1] == '']
+    option_sets = [y[selection[0]][option] for option in y[selection[0]]
+                   if option.upper() == selection[1].upper() or selection[1] == '']
     url = generate_url_from_option_sets(option_sets)
     return url
+
 
 def generate_spreadsheet_url(tab, filter_string):
     try:
         spreadsheet = os.getenv('GSHEET_ID')
-        tabs = {
-            'tracks': 0,
-            'cars': 863917880
-        }
-        spreadsheet_url = f'https://docs.google.com/spreadsheets/d/{spreadsheet}/export?format=csv&id={spreadsheet}&gid={tabs[tab]}'
-        df = pd.read_csv(spreadsheet_url)
+        url = f'https://docs.google.com/spreadsheets/d/{spreadsheet}/gviz/tq?tqx=out:csv&sheet={tab}'
+        df = pd.read_csv(url)
         df.columns = df.columns.str.lower()
         filter_string = filter_string.strip(' ')
         filters = filter_string.split(',')
         filter_queries = []
+        weighting = None
         for filter in filters:
             try:
-                if filter.find('<>')>0:
-                    a = filter.split('<>')
-                    filter_queries.append(f"not `{a[0].strip(' ')}`.str.lower().str.contains('{a[1].strip(' ')}')")
-                elif filter.find('>=')>0:
-                    a = filter.split('>=')
-                    filter_queries.append(f"`{a[0].strip(' ')}`>={a[1].strip(' ')}")
-                elif filter.find('<=')>0:
-                    a = filter.split('<=')
-                    filter_queries.append(f"`{a[0].strip(' ')}`<={a[1].strip(' ')}")
-                elif filter.find('<')>0:
-                    a = filter.split('<')
-                    filter_queries.append(f"`{a[0].strip(' ')}`<{a[1].strip(' ')}")
-                elif filter.find('>')>0:
-                    a = filter.split('>')
-                    filter_queries.append(f"`{a[0].strip(' ')}`>{a[1].strip(' ')}")
-                elif filter.find(':')>0:
-                    a = filter.split(':')
-                    filter_queries.append(f"`{a[0].strip(' ')}`.str.lower().str.contains('{a[1].strip(' ')}')")
-                elif filter.find('=')>0:
-                    a = filter.split('=')
-                    filter_queries.append(f"`{a[0].strip(' ')}`.str.lower()=='{a[1].strip(' ')}'")
+                if filter.find('|')>0:
+                    or_query = []
+                    for option in filter.split('|'):
+                        if option.find('<>')>0:
+                            a = option.split('<>')
+                            or_query.append(f"not `{a[0].strip(' ')}`.str.lower().str.contains('{a[1].strip(' ')}')")
+                        elif option.find('>=')>0:
+                            a = option.split('>=')
+                            or_query.append(f"`{a[0].strip(' ')}`>={a[1].strip(' ')}")
+                        elif option.find('<=')>0:
+                            a = option.split('<=')
+                            or_query.append(f"`{a[0].strip(' ')}`<={a[1].strip(' ')}")
+                        elif option.find('<')>0:
+                            a = option.split('<')
+                            or_query.append(f"`{a[0].strip(' ')}`<{a[1].strip(' ')}")
+                        elif option.find('>')>0:
+                            a = option.split('>')
+                            or_query.append(f"`{a[0].strip(' ')}`>{a[1].strip(' ')}")
+                        elif option.find(':')>0:
+                            a = option.split(':')
+                            or_query.append(f"`{a[0].strip(' ')}`.str.lower().str.contains('{a[1].strip(' ')}')")
+                        elif option.find('=')>0:
+                            a = option.split('=')
+                            or_query.append(f"`{a[0].strip(' ')}`.str.lower()=='{a[1].strip(' ')}'")
+                    filter_queries.append(" | ".join(or_query))
+                else:
+                    if filter.find('!weight=')>0:
+                        weighting = filter.split('=')[1]
+                    elif filter.find('<>')>0:
+                        a = filter.split('<>')
+                        filter_queries.append(f"not `{a[0].strip(' ')}`.str.lower().str.contains('{a[1].strip(' ')}')")
+                    elif filter.find('>=')>0:
+                        a = filter.split('>=')
+                        filter_queries.append(f"`{a[0].strip(' ')}`>={a[1].strip(' ')}")
+                    elif filter.find('<=')>0:
+                        a = filter.split('<=')
+                        filter_queries.append(f"`{a[0].strip(' ')}`<={a[1].strip(' ')}")
+                    elif filter.find('<')>0:
+                        a = filter.split('<')
+                        filter_queries.append(f"`{a[0].strip(' ')}`<{a[1].strip(' ')}")
+                    elif filter.find('>')>0:
+                        a = filter.split('>')
+                        filter_queries.append(f"`{a[0].strip(' ')}`>{a[1].strip(' ')}")
+                    elif filter.find(':')>0:
+                        a = filter.split(':')
+                        filter_queries.append(f"`{a[0].strip(' ')}`.str.lower().str.contains('{a[1].strip(' ')}')")
+                    elif filter.find('=')>0:
+                        a = filter.split('=')
+                        filter_queries.append(f"`{a[0].strip(' ')}`.str.lower()=='{a[1].strip(' ')}'")
             except Exception as e:
                 logger.error(str(e))
 
         filtereddf = df
         for query_string in filter_queries:
             filtereddf = filtereddf.query(query_string)
-        selections = filtereddf.fullname.array
-        option_set = [[{selection: 1} for selection in selections]]
+        selections = filtereddf.to_dict('records')
+        if weighting is None:
+            option_set = [[{selection['fullname']: 1} for selection in selections]]
+        else:
+            option_set = [[{selection['fullname']: int(selection[weighting])} for selection in selections]]
         url = generate_url_from_option_sets(option_set)
         return url
     except Exception as e:

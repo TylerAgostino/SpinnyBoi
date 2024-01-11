b679375f7b2a1b1b419f595a9a0b5b200238d960
tyleragostino <tyleragostino@gmail.com>
2024-01-11 17:49:54 -0500

    fix tests

--- tests/test_CommandHandler.py
+++ tests/test_CommandHandler.py
@@ -1,36 +1,53 @@
 import unittest
+import os
 from modules import CommandHandler
 
 
 class TestCommandHandler(unittest.IsolatedAsyncioTestCase):
-    # def async_info_test(self):
-    #     command_with_args = str("/spinfo").lower().removeprefix('/spin').strip(' ')
-    #     command = command_with_args.split(' ')[0]
-    #     args = ' '.join(command_with_args.split(' ')[1:])
-    #     if args == '':
-    #         response = CommandHandler.CommandHandler().run_command(command)
-    #     else:
-    #         response = CommandHandler.CommandHandler().run_command(command, args)
-    #     print(response)
+    def setUp(self):
+        self.command_handler = CommandHandler.CommandHandler()
+        os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # Change working directory to project root
 
-    async def test_custom(self):
+    async def async_info_test(self):
+        command_with_args = str("/spinfo").lower().removeprefix('/spin').strip(' ')
+        command = command_with_args.split(' ')[0]
+        args = ' '.join(command_with_args.split(' ')[1:])
+        if args == '':
+            response = await self.command_handler.run_command(command)
+        else:
+            response = await self.command_handler.run_command(command, args)
+        print(response)
+
+    async def test_single_sheet(self):
         command_with_args = str("/spin tracks times run=0").lower().removeprefix('/spin').strip(' ')
         command = command_with_args.split(' ')[0]
         args = ' '.join(command_with_args.split(' ')[1:])
         if args == '':
-            response = await CommandHandler.CommandHandler().run_command(command)
+            response = await self.command_handler.run_command(command)
         else:
-            response = await CommandHandler.CommandHandler().run_command(command, args)
+            response = await self.command_handler.run_command(command, args)
         print(response)
 
-    #
-    # async def test_preset(self):
-    #     command_with_args = str("/spin preset any week").lower().removeprefix('/spin').strip(' ')
-    #     command = command_with_args.split(' ')[0]
-    #     args = ' '.join(command_with_args.split(' ')[1:])
-    #     if args == '':
-    #         response = CommandHandler.CommandHandler().run_command(command)
-    #     else:
-    #         response = CommandHandler.CommandHandler().run_command(command, args)
-    #     print(response)
+    async def test_custom(self):
+        command_with_args = str("/spin custom a,b,c").lower().removeprefix('/spin').strip(' ')
+        command = command_with_args.split(' ')[0]
+        args = ' '.join(command_with_args.split(' ')[1:])
+        if args == '':
+            response = await self.command_handler.run_command(command)
+        else:
+            response = await self.command_handler.run_command(command, args)
+        print(response)
+
+    async def test_preset(self):
+        command_with_args = str("/spin preset chain").lower().removeprefix('/spin').strip(' ')
+        command = command_with_args.split(' ')[0]
+        args = ' '.join(command_with_args.split(' ')[1:])
+        if args == '':
+            response = await self.command_handler.run_command(command)
+        else:
+            response = await self.command_handler.run_command(command, args)
+        print(response)
+
+    def tearDown(self):
+        self.command_handler.driver.close()
 
